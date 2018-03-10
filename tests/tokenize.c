a2485d94daf0ac50b12656ebe558fe85d116f47e
MrVallentin <vallentinsource@gmail.com>
2018-03-10 15:32:09 +0100

    Test token ranges

--- tests/tokenize.c
+++ tests/tokenize.c
@@ -13,6 +13,10 @@ static MGbool _mgTestTokenizer(const char *in, const char *out)
 	MGTokenizer inTokenizer, outTokenizer;
 	MGToken *inToken, *outToken;
 
+#define _MG_BUFFER_LENGTH 32
+	// Warning: This could overflow, but is highly unlikely
+	char buffer[_MG_BUFFER_LENGTH];
+
 	MGbool passed = MG_TRUE;
 
 	mgCreateTokenizer(&inTokenizer);
@@ -35,6 +39,8 @@ static MGbool _mgTestTokenizer(const char *in, const char *out)
 #define _MG_FIND_NEXT_TESTABLE_TOKEN(token) \
 		for (; !_MG_IS_TESTABLE_TOKEN(token); ++token)
 
+#define _MG_TOKEN_SCAN_LINE(token) for (; token->type == MG_TOKEN_WHITESPACE; ++token)
+
 	outToken = outTokenizer.tokens;
 
 	for (inToken = inTokenizer.tokens;; ++inToken)
@@ -46,6 +52,9 @@ static MGbool _mgTestTokenizer(const char *in, const char *out)
 
 		if (outToken->type == MG_TOKEN_EOF)
 		{
+			if (inToken->type == MG_TOKEN_EOF)
+				break;
+
 			printf("Error: Unexpected token...\n");
 			mgDebugInspectToken(inToken, inTokenizer.filename, MG_FALSE);
 			goto fail;
@@ -66,11 +75,10 @@ static MGbool _mgTestTokenizer(const char *in, const char *out)
 			goto fail;
 		}
 
-		do
-			++outToken;
-		while (outToken->type == MG_TOKEN_WHITESPACE);
+		++outToken;
+		_MG_TOKEN_SCAN_LINE(outToken);
 
-		if (_MG_IS_TESTABLE_TOKEN(outToken) && (outToken->type != MG_TOKEN_EOF))
+		if (outToken->type == MG_TOKEN_STRING)
 		{
 			const char *outTokenString = outToken->value.s;
 			const size_t outTokenStringLength = outTokenString ? strlen(outTokenString) : 0;
@@ -101,10 +109,88 @@ static MGbool _mgTestTokenizer(const char *in, const char *out)
 			}
 
 			++outToken;
+			_MG_TOKEN_SCAN_LINE(outToken);
+		}
+
+		if (outToken->type == MG_TOKEN_INTEGER)
+		{
+			snprintf(buffer, _MG_BUFFER_LENGTH, "%u", inToken->begin.line);
+
+			if (strncmp(buffer, outToken->begin.string, outToken->end.string - outToken->begin.string))
+			{
+				printf("Error: Unexpected begin line %u, expected %.*s\n",
+				       inToken->begin.line, outToken->end.string - outToken->begin.string, outToken->begin.string);
+				mgDebugInspectToken(inToken, inTokenizer.filename, MG_FALSE);
+				goto fail;
+			}
+
+			++outToken;
+			_MG_TOKEN_SCAN_LINE(outToken);
+		}
+
+		if (outToken->type == MG_TOKEN_COLON)
+		{
+			++outToken;
+			_MG_TOKEN_SCAN_LINE(outToken);
+
+			if (outToken->type == MG_TOKEN_INTEGER)
+			{
+				snprintf(buffer, _MG_BUFFER_LENGTH, "%u", inToken->begin.character);
+
+				if (strncmp(buffer, outToken->begin.string, outToken->end.string - outToken->begin.string))
+				{
+					printf("Error: Unexpected begin character %u, expected %.*s\n",
+					       inToken->begin.character, outToken->end.string - outToken->begin.string, outToken->begin.string);
+					mgDebugInspectToken(inToken, inTokenizer.filename, MG_FALSE);
+					goto fail;
+				}
+
+				++outToken;
+				_MG_TOKEN_SCAN_LINE(outToken);
+			}
+		}
+
+		if (outToken->type == MG_TOKEN_INTEGER)
+		{
+			snprintf(buffer, _MG_BUFFER_LENGTH, "%u", inToken->end.line);
+
+			if (strncmp(buffer, outToken->begin.string, outToken->end.string - outToken->begin.string))
+			{
+				printf("Error: Unexpected end line %u, expected %.*s\n",
+				       inToken->end.line, outToken->end.string - outToken->begin.string, outToken->begin.string);
+				mgDebugInspectToken(inToken, inTokenizer.filename, MG_FALSE);
+				goto fail;
+			}
+
+			++outToken;
+			_MG_TOKEN_SCAN_LINE(outToken);
+		}
+
+		if (outToken->type == MG_TOKEN_COLON)
+		{
+			++outToken;
+			_MG_TOKEN_SCAN_LINE(outToken);
+
+			if (outToken->type == MG_TOKEN_INTEGER)
+			{
+				snprintf(buffer, _MG_BUFFER_LENGTH, "%u", inToken->end.character);
+
+				if (strncmp(buffer, outToken->begin.string, outToken->end.string - outToken->begin.string))
+				{
+					printf("Error: Unexpected end character %u, expected %.*s\n",
+					       inToken->end.character, outToken->end.string - outToken->begin.string, outToken->begin.string);
+					mgDebugInspectToken(inToken, inTokenizer.filename, MG_FALSE);
+					goto fail;
+				}
+
+				++outToken;
+			}
 		}
 
 		if (inToken->type == MG_TOKEN_EOF)
 			break;
+
+		for (; (outToken->type != MG_TOKEN_NEWLINE) && (outToken->type != MG_TOKEN_EOF); ++outToken);
 	}
 
 	_MG_FIND_NEXT_TESTABLE_TOKEN(outToken);
@@ -116,8 +202,10 @@ static MGbool _mgTestTokenizer(const char *in, const char *out)
 		goto fail;
 	}
 
+#undef _MG_BUFFER_LENGTH
 #undef _MG_IS_TESTABLE_TOKEN
 #undef _MG_FIND_NEXT_TESTABLE_TOKEN
+#undef _MG_TOKEN_SCAN_LINE
 
 	goto pass;
 
